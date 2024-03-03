1230d5c835ab8f03a53237bc99dc0183e155014f
ilana <NoamZecharia1808@gmail.com>
2024-03-03 23:49:22 +0200

    There are some problems in the loading sign

--- app/src/main/java/com/example/supermarket/clean/CleanFragment.java
+++ app/src/main/java/com/example/supermarket/clean/CleanFragment.java
@@ -1,15 +1,20 @@
 package com.example.supermarket.clean;
 
+import android.annotation.SuppressLint;
 import android.content.ClipData;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.supermarket.FireBase;
 import com.example.supermarket.MyAdapter;
@@ -27,6 +32,8 @@ public class CleanFragment extends Fragment {
     private ArrayList<byte[]> photos;
     private String[] description;
     private String[] title;
+    private ProgressBar progressBar;
+    private Button btn;
 
     public static CleanFragment newInstance(String param1, String param2) {
         CleanFragment fragment = new CleanFragment();
@@ -37,16 +44,22 @@ public class CleanFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_alcohol, container, false);
-
+        View root = inflater.inflate(R.layout.fragment_clean, container, false);
+        progressBar = root.findViewById(R.id.progressBar);
+        btn = root.findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                download(getView());
+            }
+        });
         list = root.findViewById(R.id.list);
         fireBase = new FireBase(getContext());
-
         fireBase.getInformation("Clean",value -> {
             if (value instanceof ArrayList<?>){
                 products = (ArrayList<ProductSec>) value;
@@ -77,5 +90,30 @@ public class CleanFragment extends Fragment {
         });
 
         return root;
+    }
+    public void download(View view){
+        CountDownTimer countDownTimer = new CountDownTimer(10000,500) {
+            @Override
+            public void onTick(long l) {
+                int current = progressBar.getProgress();
+                int secondsCurrent = progressBar.getSecondaryProgress();
+                if(current < progressBar.getMax()){
+                    progressBar.setProgress(current + 10);
+                }else{
+                    progressBar.setProgress(0);
+                }
+                if(secondsCurrent < progressBar.getMax()){
+                    progressBar.setSecondaryProgress(current + 5);
+                }else{
+                    progressBar.setSecondaryProgress(0);
+                }
+            }
+
+            @Override
+            public void onFinish() {
+                Toast.makeText(getContext(), "Finish", Toast.LENGTH_SHORT).show();
+            }
+        };
+        countDownTimer.start();
     }
 }
--- app/src/main/java/com/example/supermarket/fish/FishFragment.java
+++ app/src/main/java/com/example/supermarket/fish/FishFragment.java
@@ -2,6 +2,7 @@ package com.example.supermarket.fish;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,28 +12,42 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
+import com.example.supermarket.FireBase;
+import com.example.supermarket.MyAdapter;
+import com.example.supermarket.ProductSec;
 import com.example.supermarket.R;
+
+import java.util.ArrayList;
 
 public class FishFragment extends Fragment {
 
-    private FishViewModel mViewModel;
+    private FireBase fireBase;
+    private ListView list;
+    private ArrayList<ProductSec> products;
+    private double[] price;
+    private ArrayList<byte[]> photos;
+    private String[] description;
+    private String[] title;
 
-    public static FishFragment newInstance() {
-        return new FishFragment();
+    public static FishFragment newInstance(String param1, String param2) {
+        FishFragment fragment = new FishFragment();
+        return fragment;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_fish, container, false);
+        View root =  inflater.inflate(R.layout.fragment_fish, container, false);
+        list = root.findViewById(R.id.list);
+        fireBase = new FireBase(getContext());
+        return root;
     }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(FishViewModel.class);
-        // TODO: Use the ViewModel
-    }
-
 }
